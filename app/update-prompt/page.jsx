fc358857e9996f38c5d0b66046575555b733d0dc
Vasu <vasukumar962@gmail.com>
2024-03-22 17:52:20 +0530

    fix the code

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,20 +1,31 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-
+import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/dist/client/router';
 import Form from '/components/Form';
 
 const EditPrompt = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
-
+  const [searchParamsLoaded, setSearchParamsLoaded] = useState(false);
+  const [promptId, setPromptId] = useState(null);
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
   });
+
+  useEffect(() => {
+    setSearchParamsLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (searchParamsLoaded) {
+      const params = new URLSearchParams(window.location.search);
+      const id = params.get('id');
+      setPromptId(id);
+    }
+  }, [searchParamsLoaded]);
 
   useEffect(() => {
     const getPromptDetails = async () => {
@@ -32,7 +43,7 @@ const EditPrompt = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert('Prompt ID not found!')
+    if (!promptId) return alert('Prompt ID not found!');
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
