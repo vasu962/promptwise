911b9ee184da17e4d39864570d29114664623d98
Vasu <vasukumar962@gmail.com>
2024-03-24 19:50:24 +0530

    fixed errors in update-prompt file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,21 @@
-
-import Nav from "/components/Nav";
-import Provider from "/components/Provider";
-import "/styles/globals.css";
+import Nav from '/components/Nav';
+import Provider from '/components/Provider';
+import '/styles/globals.css';
 
 export const metadata = {
-  title: "Promptwise",
-  description: "Discover & Share AI Prompts",
+  title: 'Promptwise',
+  description: 'Discover & Share AI Prompts',
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
+  <html lang="en">
     <body>
       <Provider>
-        <div className='main'>
-          <div className='gradient' />
+        <div className="main">
+          <div className="gradient" />
         </div>
 
-        <main className='app'>
+        <main className="app">
           <Nav />
           {children}
         </main>
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '/components/Form';
@@ -32,7 +32,7 @@ const EditPrompt = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert('Prompt ID not found!')
+    if (!promptId) return alert('Prompt ID not found!');
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -53,13 +53,15 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
